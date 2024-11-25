0983de877fa0b2756b7bf5ee58868e57128f2f05
makar <makar.peter98@gmail.com>
2024-11-25 14:23:03 +0100

    inital commit

---
+++ src/users/AddPerson.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+export default function AddPerson() {
+  
+  return ;
+}
---
+++ src/users/EditPerson.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Link, useNavigate, useParams } from "react-router-dom";
+
+export default function EditPerson() {
+  
+  return ;
+}
